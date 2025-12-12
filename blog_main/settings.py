7369b745c232a653f8445d1822df231eb1d52877
Himanshu <himanshsharma102@gmail.com>
2025-12-12 15:33:24 +0530

    search funtionality

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
+    'about',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'blogs.context_processors.get_categories',
+                'blogs.context_processors.get_social_link',
             ],
         },
     },
@@ -116,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     'blog_main/static',
--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,5 +1,9 @@
 from .models import Category
-
+from about.models import SocialLink
 def get_categories(request):
     categories = Category.objects.all()
     return dict(categories=categories)
+
+def get_social_link(request):
+    social_link = SocialLink.objects.all()
+    return dict(social_link = social_link )
